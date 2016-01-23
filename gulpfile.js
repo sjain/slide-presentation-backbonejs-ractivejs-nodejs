65de1ec6953a38837c47394e2817111ec5de1b2b
Sharad Jain <shaanjain@gmail.com>
2016-01-22 23:24:26 -0600

    Use browserify to require backbone and ractive. Watch still not working.

--- gulpfile.js
+++ gulpfile.js
@@ -4,19 +4,28 @@ var sass   = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 var concat = require('gulp-concat');
 var browserSync = require('browser-sync').create();
+var browserify = require('gulp-browserify');
+var gutil = require('gulp-util');
+var rename = require('gulp-rename');
+
+global.WATCH_MODE = false;
+global.PRODUCTION_MODE = (gutil.env.type === 'production');
 
 var config = function () {
   var jsDir = './src/js';
   var scssDir = './src/scss';
 
   var config = {
+    jsDir: jsDir,
     jsFiles: [
       jsDir + '/**/*.js',
     ],
+    scssDir: scssDir,
     scssFiles: [
       scssDir + '/**/*.scss',
     ],
     destDir: './',
+    bundleJsFile: './app.js',
   };
 
   return config;
@@ -30,13 +39,21 @@ gulp.task('jshint', function() {
     .pipe(jshint.reporter('jshint-stylish'));
 });
 
-gulp.task('scripts', function() {
-  return gulp.src(config.jsFiles)
-    .pipe(sourcemaps.init())
-    .pipe(concat('app.js'))
-    .pipe(sourcemaps.write())
+gulp.task('browserify', function() {
+  gulp.src(config.jsFiles, {read: false})
+
+    // Browserify, and add source maps if this isn't a production build
+    .pipe(browserify({
+      debug: !PRODUCTION_MODE,
+      //transform: ['reactify'],
+      //extensions: ['.jsx']
+      paths: ['./node_modules', config.jsDir],
+      watcher: WATCH_MODE,
+    }))
+    .pipe(rename(config.bundleJsFile))
     .pipe(gulp.dest(config.destDir));
 });
+
 
 gulp.task('styles', function() {
   return gulp.src(config.scssFiles)
@@ -48,13 +65,13 @@ gulp.task('styles', function() {
     .pipe(browserSync.stream());
 });
 
-gulp.task('scripts-watch', browserSync.reload);
 gulp.task('watch', function() {
+  WATCH_MODE = true;
   browserSync.init({
     server: {
       baseDir: "./"
     }
   });
-  gulp.watch(config.jsFiles, ['jshint', 'scripts', 'scripts-watch']);
+  gulp.watch(config.jsFiles, ['jshint', 'browserify']);
   gulp.watch(config.scssFiles, ['styles']);
 });
--- src/js/test.js
+++ src/js/test.js
@@ -1,4 +1,22 @@
-function app() {
-  console.log('Hi!');
-}
+var Backbone = require('backbone');
+var Ractive = require('ractive');
+var backboneAdaptor = require('ractive-adaptors-backbone');
+
+backboneAdaptor.Backbone = Backbone;
+
+global.app = function() {
+
+  var user = new Backbone.Model({
+    name: 'User'
+  });
+
+  var ractive = new Ractive({
+    el: '#main',
+    template: '<h1>Hello {{user.name}}!</h1>',
+    data: {
+      user: user
+    },
+    adapt: [ backboneAdaptor ]
+  });
+};
 
