728335ee8388fde0b5c34ef2e683cfa2c0105e59
Sharad Jain <shaanjain@gmail.com>
2016-01-25 23:22:36 -0600

    anonymous components to organize view code.

--- src/js/views/deck_list.js
+++ src/js/views/deck_list.js
@@ -3,16 +3,23 @@ var Backbone = require('backbone');
 var backboneAdaptor = require('ractive-adaptors-backbone');
 backboneAdaptor.Backbone = Backbone;
 
+var componentTemplate = '<title/><list/>';
+var titleTemplate = '<h1>There are {{decks.length}} decks!</h1>';
+var listTemplate = "<ul>{{#each decks}}<list-item />{{/each}}</ul>";
+var listItemTemplate = "<li>{{name}}</li>";
+
 var DeckList = Ractive.extend({
   isolated: false,
-  template: '<h1>There are {{decks.length}} decks!</h1>' +
-    '<table><thead><td>Name</td></thead>' +
-    '{{#each decks}}<tr><td>{{name}}</td></tr>{{/each}}' +
-    '</table>',
+  template: componentTemplate,
   data: function() {
    return {
      decks: [],
    };
+  },
+  components: {
+    title: Ractive.extend({template: titleTemplate}),
+    list: Ractive.extend({template: listTemplate}),
+    'list-item': Ractive.extend({template: listItemTemplate})
   },
   adapt: [ backboneAdaptor ]
 });
