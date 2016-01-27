f95386c8359b2854c76e4f895f4aaf6b57e41db4
Sharad Jain <shaanjain@gmail.com>
2016-01-27 01:00:38 -0600

    slide previous/next links

--- src/js/views/slide_show.js
+++ src/js/views/slide_show.js
@@ -9,7 +9,10 @@ var titleTemplate = '<h1>{{slide.deck}}</h1>';
 var headingTemplate = '<h4>{{slide.slide[0].heading}}</h4>';
 var paragraphTemplate = '<p>{{slide.slide[0].para}}</p>';
 var imageTemplate = '<img src="{{slide.slide[0].image}}"/>';
-var footerTemplate = '<h4>Slide {{slide.id}} of {{slide.slides}}</h4>'
+var footerTemplate = '<ul class="footer"><previousSlideLink/><currentSlideInfo/><nextSlideLink/></ul>';
+var previousSlideLink = '<li><a href="/decks/{{slide.deck}}/slides/{{ parseInt(slide.id)-1}}">&lt;&lt;</a></li>';
+var currentSlideInfo = '<li>Slide {{slide.id}} of {{slide.slides}}</li>';
+var nextSlideLink = '<li><a href="/decks/{{slide.deck}}/slides/{{ parseInt(slide.id) + 1 }}">&gt;&gt;</a></li>';
 
 var SlideShow = Ractive.extend({
   isolated: false,
@@ -25,8 +28,12 @@ var SlideShow = Ractive.extend({
     paragraph: Ractive.extend({template: paragraphTemplate}),
     image: Ractive.extend({template: imageTemplate}),
     footer: Ractive.extend({template: footerTemplate}),
+    previousSlideLink: Ractive.extend({template: previousSlideLink}),
+    currentSlideInfo: Ractive.extend({template: currentSlideInfo}),
+    nextSlideLink: Ractive.extend({template: nextSlideLink}),
   },
   adapt: [ backboneAdaptor ],
 });
 
 module.exports = SlideShow;
+
