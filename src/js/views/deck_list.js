f49215993e982504b62ea4e5b8bd9127322bcd60
Sharad Jain <shaanjain@gmail.com>
2016-01-26 23:18:37 -0600

    attempt-1 for link click handler: incomplete gets called each time causes cumulative event firing.

--- src/js/views/deck_list.js
+++ src/js/views/deck_list.js
@@ -1,4 +1,5 @@
 var Ractive = require('ractive');
+var $ = require('jquery');
 var Backbone = require('backbone');
 var backboneAdaptor = require('ractive-adaptors-backbone');
 backboneAdaptor.Backbone = Backbone;
@@ -8,7 +9,7 @@ var Decks = require('../models/decks');
 var componentTemplate = '<title/><list/>';
 var titleTemplate = '<h1>There are {{decks.length}} decks!</h1>';
 var listTemplate = "<ul>{{#each decks}}<list-item />{{/each}}</ul>";
-var listItemTemplate = "<li>{{name}}</li>";
+var listItemTemplate = "<li><a href='/decks/{{name}}'>{{name}}</a></li>";
 
 var decks = new Decks();
 
@@ -25,7 +26,19 @@ var DeckList = Ractive.extend({
     list: Ractive.extend({template: listTemplate}),
     'list-item': Ractive.extend({template: listItemTemplate})
   },
-  adapt: [ backboneAdaptor ]
+  adapt: [ backboneAdaptor ],
+  oncomplete: function() {
+    $(document).on("click", "a:not([data-bypass])", function(evt) {
+      var href = { prop: $(this).prop("href"), attr: $(this).attr("href") };
+      console.log('href:', href);
+      var root = location.protocol + "//" + location.host + Backbone.history.options.root;
+
+      if (href.prop && href.prop.slice(0, root.length) === root) {
+        evt.preventDefault();
+        Backbone.history.navigate(href.attr, true);
+      }
+    });
+  }
 });
 
 module.exports = DeckList;
