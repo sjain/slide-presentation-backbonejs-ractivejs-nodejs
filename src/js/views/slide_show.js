1c9d7426485b37df51b3f8b42cfe7b9705ff8678
Sharad Jain <shaanjain@gmail.com>
2016-01-27 10:12:27 -0600

    return to deck list when moving before first. Display stats when moving past last slide.

--- src/js/views/slide_show.js
+++ src/js/views/slide_show.js
@@ -4,15 +4,20 @@ var backboneAdaptor = require('ractive-adaptors-backbone');
 backboneAdaptor.Backbone = Backbone;
 
 
+// FIXME These template snippets are not scalable. Figure out external HTML template files..
 var componentTemplate = '<title/><heading/><paragraph/><image/><footer/>';
 var titleTemplate = '<h1>{{slide.deck}}</h1>';
 var headingTemplate = '<h4>{{slide.slide[0].heading}}</h4>';
 var paragraphTemplate = '<p>{{slide.slide[0].para}}</p>';
 var imageTemplate = '<img src="{{slide.slide[0].image}}"/>';
-var footerTemplate = '<ul class="footer"><previousSlideLink/><currentSlideInfo/><nextSlideLink/></ul>';
+var footerTemplate = '<ul class="footer"><previousLink/><currentSlideInfo/><nextLink/></ul>';
+var previousLink = '{{#if parseInt(slide.id) != 1}}<previousSlideLink/>{{/if}}{{#if parseInt(slide.id) == 1}}<returnToDeckListLink/>{{/if}}';
+var returnToDeckListLink = '<li><a href="/decks">All Decks</a></li>';
 var previousSlideLink = '<li><a href="/decks/{{slide.deck}}/slides/{{ parseInt(slide.id)-1}}">&lt;&lt;</a></li>';
 var currentSlideInfo = '<li>Slide {{slide.id}} of {{slide.slides}}</li>';
+var nextLink = '{{#if parseInt(slide.id) != slide.slides}}<nextSlideLink/>{{/if}}{{#if parseInt(slide.id) == slide.slides}}<deckStatsLink/>{{/if}}';
 var nextSlideLink = '<li><a href="/decks/{{slide.deck}}/slides/{{ parseInt(slide.id) + 1 }}">&gt;&gt;</a></li>';
+var deckStatsLink = '<li><a href="/decks/{{slide.deck}}/stats">Finish</a></li>';
 
 var SlideShow = Ractive.extend({
   isolated: false,
@@ -28,9 +33,13 @@ var SlideShow = Ractive.extend({
     paragraph: Ractive.extend({template: paragraphTemplate}),
     image: Ractive.extend({template: imageTemplate}),
     footer: Ractive.extend({template: footerTemplate}),
+    previousLink: Ractive.extend({template: previousLink}),
     previousSlideLink: Ractive.extend({template: previousSlideLink}),
+    returnToDeckListLink: Ractive.extend({template: returnToDeckListLink}),
     currentSlideInfo: Ractive.extend({template: currentSlideInfo}),
+    nextLink: Ractive.extend({template: nextLink}),
     nextSlideLink: Ractive.extend({template: nextSlideLink}),
+    deckStatsLink: Ractive.extend({template: deckStatsLink}),
   },
   adapt: [ backboneAdaptor ],
 });
